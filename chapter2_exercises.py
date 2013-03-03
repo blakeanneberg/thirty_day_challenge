3b62d1bd628df54fae06faf4e9becedb1ba53c99
blakeanneberg <blake.anneberg@gmail.com>
2013-03-03 16:25:31 -0500

    30 day challenge

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,39 @@
-# Exercises for chapter 2: Problems 2.1, 2.2, 2.3, and 2.4 in Think Python
+print 'Blake Anneberg Homework Chapter 2'
 
-# Name
+print "Exercise 2.1" # need to convert the decimal 02492 to Octal
+zipcode =  00000,0002,004,011,02 
+print zipcode
+
+print "Exercise 2.2" #not sure of what the question is asking... :-/
+x = 3
+print x + 3 
+
+print "Exerciese 2.3"
+
+width = 17
+height = 12.0 
+delimiter = '.'
+print width/2
+print width/2.0
+print height/3
+print 1 + 2 * 5
+delimiter * 5
+
+print "Exerciese 2.4"
+print "Question 1"
+import math
+print .75 * math.pi * (5*5*5) 
+print "Question 2"
+total = ((24.95/.40) + 3) + ((24.95/.40) + .75) * 60
+total = 'The total is $'+str(total)
+print total
+
+print "Question 3"
+
+minutes =  8 + 7 + 7 + 7 + 8 + 1 
+seconds = 15 + 12 + 12 + 12 + 15 - 60
+real_minutes = (minutes + 52) - 60 # Gets 30 minutes, 
+hours = 6 + 1 #6 hours plus 1 hour from above
+real_time = hours , real_minutes , seconds
+print "The time the runner finished her/his run was HH MM SS:" +str(real_time)
+
--- chapter3_exercises.py
+++ chapter3_exercises.py
@@ -1,3 +1,40 @@
-# Exercises for chapter 3: Problems 3.1, 3.2, 3.3, and 3.4 in Think Python
+print 'Blake Anneberg Homework Chapter 2'
 
-# Name
+print "Exerciese 3.1"
+#repeat_lyrics() #NameError: name 'repeat_lyrics' is not defined
+
+
+print "Exerciese 3.2"
+
+#Traceback (most recent call last):
+#  File "/Users/banneberg/chapter3_exercises.py", line 15, in <module>
+#    repeat_lyrics()
+#  File "/Users/banneberg/chapter3_exercises.py", line 12, in repeat_lyrics
+#    print_lyrics()
+#NameError: global name 'print_lyrics' is not defined
+
+
+print "Exerciese 3.3"
+right_justify ='                                                                 allen'
+print len('                                                                 allen')
+print right_justify
+
+print "Exerciese 3.4" #had to look up how to do this, was kinda confusing...
+
+def do_twice(f, arg):
+	f(arg)
+	f(arg)
+
+def print_twice(arg):
+    print arg
+    print arg
+
+do_twice(print_twice, 'spam')
+print ''
+
+def do_four(f, arg):
+	do_twice(f, arg)
+	do_twice(f, arg)
+
+do_four(print_twice, 'spam')
+print ''
